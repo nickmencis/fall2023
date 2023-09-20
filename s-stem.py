fb946162ce7f462f7206c2b82e0c09bc21f6da92
mencisnj <nickjmencis@gmail.com>
2023-09-20 19:41:28 -0400

    total_stars.csv has been edited for mor uniform headings s-stem.py has not been fully run/debugged

--- s-stem.py
+++ s-stem.py
@@ -1,1 +1,65 @@
-'Creators: Nick Mencis, Sydney Andrews, Sitara Brent'
+"""
+AUTHORS: Nick Mencis, Sydney Andrews, Sitara Brent
+CREATED: 20 September 2023
+S-STEM
+FALL 2023
+CODING: UTF-8
+"""
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# 1. Load the dataset
+data = pd.read_csv('total_stars.csv')
+
+# Split the data into training and testing sets
+X = data[['Mass', 'Radius', 'Luminosity']]
+y = data['Temperature']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predict the temperatures for the test set
+y_pred = model.predict(X_test)
+
+# Calculate the RMSE for evaluation
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+print(f'Root Mean Squared Error: {rmse}')
+
+# Predict temperatures for the whole dataset
+data['Predicted_Temperature'] = model.predict(X)
+
+# 2. Classify based on the temperature
+def classify_spectral_type(temp):
+    if temp > 30000:
+        return 'O'
+    elif temp > 10000:
+        return 'B'
+    elif temp > 7500:
+        return 'A'
+    elif temp > 6000:
+        return 'F'
+    elif temp > 5200:
+        return 'G'
+    elif temp > 3700:
+        return 'K'
+    else:
+        return 'M'
+
+# You can expand this for Luminosity Classification
+def classify_luminosity(temp):
+    # Dummy classification based on temperature, adjust as necessary
+    if temp > 15000:
+        return 'I'
+    else:
+        return 'V'
+
+data['Spectral_Type'] = data['Predicted_Temperature'].apply(classify_spectral_type)
+data['Luminosity_Classification'] = data['Predicted_Temperature'].apply(classify_luminosity)
+
+# 3. Write to CSV
+data.to_csv('ml_stars.csv', index=False)
